efc0a17298758c24c3c35a7850e6f836fe82c788
Zach <zlederman00@gmail.com>
2022-12-30 11:23:23 -0600

    implemented request authorization

--- sms-entrypoint/utils/SMSHandler.ts
+++ sms-entrypoint/utils/SMSHandler.ts
@@ -1,43 +1,29 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import {Twilio, twiml, validateRequest} from 'twilio'
-import {parse} from 'querystring'
 import {ParsedSMS} from './types'
 import { TWILIO_SID, TWILIO_TOKEN, TWILIO_NUMBER,TWILIO_HOOK_URL} from "./constants";
 const {MessagingResponse} = twiml
 
-// const validateReq = (req: HttpRequest) => {
-
-//     let twilioHeader = req.headers['x-twilio-signature']
-//     if(twilioHeader !== undefined){
-//         return validateRequest(TWILIO_TOKEN,twilioHeader,'https://budgetbud.xyz/api/sms',parse(req.body))
-//     }
-//     else{
-//         throw new Error();
-//     }
-// }
+const validateReq = (req: HttpRequest) => {
+    let twilioHeader = req.headers['x-twilio-signature']
+    if(twilioHeader !== undefined){
+        return validateRequest(TWILIO_TOKEN,twilioHeader,req.url,{})
+    }
+    return false
+} 
 
 export const SMSHandler: AzureFunction = async (context: Context, req: HttpRequest) => {
-    try{
-        context.log(req.headers['x-twilio-signature'])
-        // const isValid = validateReq(req)
-        // context.log(isValid)
-        const message = req.body.Body;
-
-        const response = new MessagingResponse();
-        response.message(`Hello from TypeScript! You said "${message}"`);
-        
-        context.res.set("Content-Type", "application/xml");
-        context.res.send(response.toString());
-
-
-    }catch(e){
+    context.log(`${req.query.From} - ${req.query.Body}`)
+    const isValid = validateReq(req)
+    if(!isValid){
+        context.log(`unauthorized - ${JSON.stringify(req)}`)
         context.res.status = 401
-        context.res.send(e)
+        context.res.send('Unauthorized')
+        return
     }
-    
-    // const sms: SMS = {
-    //     body: req.body.Body,
-    //     phone: req.body.From,
-    //     timeStamp: Date.now()
-    // }
+    context.log(`authorized!`)
+    let response = new MessagingResponse()
+    response.message(`Authorized - ${req.query.Body}`)
+    context.res.set('Content-Type','application/xml')
+    context.res.send(response.toString())
 }
--- sms-entrypoint/utils/constants.ts
+++ sms-entrypoint/utils/constants.ts
@@ -1,3 +1,4 @@
+
 export const TWILIO_SID = process.env.TWILIO_SID
 export const TWILIO_TOKEN = process.env.TWILIO_ACCOUNT_TOKEN
 export const TWILIO_NUMBER = process.env.TWILIO_NUMBER
